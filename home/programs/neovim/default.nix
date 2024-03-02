5dda99f71ba414009b97744c93c236f914429d6f
4Lost <elias.schroeter@e.email>
2024-03-02 12:57:54 +0100

    nvim: theme & lazy-nvim

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,12 +1,49 @@
 { pkgs, config, ... }:
 
 {
-  #home.packages = with pkgs; [
-    #neovim
-  #];
+  home.sessionVariables = { EDITOR = "nvim"; };
 
   programs.neovim = {
     enable = true;
+
+  plugin = with pkgs.vimPlugins; [
+    lazy-nvim
+    
+    catppuccin-nvim
+  ];
+
+  extraLuaConfig = ''
+    vim.g.mapleader = " "
+      require("lazy").setup({
+        spec = {
+          -- Import plugins from lua/plugins
+          { import = "plugins" },
+        },
+        performance = {
+          reset_packpath = false,
+          rtp = {
+              reset = false,
+            }
+          },
+        dev = {
+          path = "${
+            pkgs.vimUtils.packDir
+            config.programs.neovim.finalPackage.passthru.packpathDirs
+          }/pack/myNeovimPackages/start",
+          patterns = {"folke", "catppuccin"},
+        },
+        install = {
+          -- Safeguard in case we forget to install a plugin with Nix
+          missing = false,
+        },
+      })
+      require("config/options")
+  '';
+  };
+
+  xdg.configFile."nvim/lua"= {
+    recursive = true;
+    source = ./lua;
   };
 }
 
---
+++ home/programs/neovim/lua/config/options.lua
@@ -0,0 +1,14 @@
+local opt = vim.opt
+
+opt.cursorline = true -- Enable highlighting of the current line
+opt.expandtab = true -- Use spaces instead of tabs
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.relativenumber = true -- Relative line numbers
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2 -- Size of an indent
+opt.smartindent = true -- Insert indents automatically
+opt.spelllang = { "en" }
+opt.tabstop = 2 -- Number of spaces tabs count for
+
