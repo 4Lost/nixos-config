ce176d00227b6a6f2914379fe481ca172830290a
4Lost <elias.schroeter@e.email>
2026-02-23 16:40:28 +0100

    neovim cleanup

--- home/programs/nixvim/default.nix
+++ home/programs/nixvim/default.nix
@@ -30,9 +30,9 @@
     extraPackages = with pkgs; [
       texliveFull
       texlivePackages.standalone
+      texlivePackages.relsize # needed for BA
       # papis-nvim
-    ]
-    ;
+    ];
     extraConfigVim = ''
       filetype plugin on
       set nocompatible
